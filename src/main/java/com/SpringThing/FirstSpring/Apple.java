bc5af121a78b66376910b48805de16b39608c5e4
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-10-11 20:57:03 +0530

    Bean declared using Configuration annotation

---
+++ src/main/java/com/SpringThing/FirstSpring/Apple.java
@@ -0,0 +1,16 @@
+package com.SpringThing.FirstSpring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Apple {
+    @Bean
+    public String getApple(){
+        return "This is MiniBean";
+    }
+    @Bean
+    public String eatApple(){
+        return "Eating Apple from Mini Bean";
+    }
+}
--- src/main/java/com/SpringThing/FirstSpring/FirstSpringApplication.java
+++ src/main/java/com/SpringThing/FirstSpring/FirstSpringApplication.java
@@ -1,13 +1,23 @@
 package com.SpringThing.FirstSpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstSpringApplication {
+public class FirstSpringApplication implements CommandLineRunner {
+
+	@Autowired
+	Apple apple;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(apple.getApple());
+		System.out.println(apple.eatApple());
+	}
 }
